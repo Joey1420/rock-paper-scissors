4d40c8a5568ddf065d7c3cda84a1fedb9dcc21c4
Joey1420 <stefaniwjoey@gmail.com>
2022-12-17 23:58:31 -0500

    Moved variable inside function and started if else statement

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,8 @@
-const choice = ["rock", "paper", "scissors"];
-
 function computerChoice(max){
+    const choice = ["rock", "paper", "scissors"];
     return Math.floor(Math.random(0) * 3);
+}
+
+if (computerChoice === playerChoice){
+    alert = "Draw!";
 }
