76be48bb00c0bdf81a3f33963cd33c5679fbde26
Joey1420 <stefaniwjoey@gmail.com>
2022-12-19 23:00:54 -0500

    Added comment for computer choices

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,10 @@
+// computer choices
 function computerChoice(max){
     const choice = ["rock", "paper", "scissors"];
     return Math.floor(Math.random(0) * 3);
 }
-
+function chooseWinner{
 if (computerChoice === playerChoice){
-    alert = "Draw!";
+    return = "Draw!";
+  }
 }
