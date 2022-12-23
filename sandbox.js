de84064d3657e11565811c11ccabe04a4d36ca51
Joey1420 <stefaniwjoey@gmail.com>
2022-12-22 23:20:23 -0500

    Added comment for if else functions

--- sandbox.js
+++ sandbox.js
@@ -3,6 +3,7 @@ function computerChoice(max){
     const choice = ["rock", "paper", "scissors"];
     return Math.floor(Math.random(0) * 3);
 }
+// if else functions for game
 function chooseWinner{
 if (computerChoice === playerChoice){
     return = "Draw!";
