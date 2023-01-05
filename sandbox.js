67c4b9fb0454bc6d30e6501d8e146746d8ad4c18
Joey1420 <stefaniwjoey@gmail.com>
2023-01-04 23:38:06 -0500

    Set all functions for winning the game

--- sandbox.js
+++ sandbox.js
@@ -6,6 +6,30 @@ function computerChoice(max){
 // if else functions for game
 function chooseWinner() {
 if (computerChoice === playerChoice){
-    return console.log ("Draw!");
+    return console.log ("Draw!"); 
+  }
+}
+
+if (playerChoice === "rock"){
+    if (computerChoice === "paper"){
+    return console.log ("You lose!");
+    } else { 
+    console.log ("You win!");
+  }
+}
+
+if (playerChoice === "scissors"){
+  if (computerChoice === "rock"){
+    return console.log ("You lose!");
+  } else {
+    console.log("You win!");
+  }
+}
+
+if (playerChoice === "paper"){
+  if (computerChoice === "scissors"){
+    return console.log ("You lose!");
+  } else {
+    console.log("You win!");
   }
 }
